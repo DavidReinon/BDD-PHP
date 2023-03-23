70c36787d7c8f95b6c4eb526ad8165dc653c6710
DavidReinon <darega@floridauniversitaria.es>
2023-03-23 16:10:01 +0100

    borrarCookie Formateo

--- Cookies/2.cookies/borrarcookie.php
+++ Cookies/2.cookies/borrarcookie.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 </head>
-<body>
-<?php
-setcookie("idioma","__",time()-1000,"/");
-echo "Se borró la cookie<br>";
 
-?>
-<a href="pagina1.php">Volver Seleccion Idioma</a>
+<body>
+	<?php
+	setcookie("idioma", "__", time() - 1000, "/");
+	echo "Se borró la cookie<br>";
+
+	?>
+	<a href="pagina1.php">Volver Seleccion Idioma</a>
 </body>
+
 </html>
