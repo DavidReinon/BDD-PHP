993c0c73a6c947afca8fdec0a03a02d200245e8f
DavidReinon <darega@floridauniversitaria.es>
2023-04-18 18:30:18 +0200

    infoselect

---
+++ BD/InfoSelect.php
@@ -0,0 +1,53 @@
+<html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
+            <title>Document</title>
+        </head>
+        <body>
+            <form id="form1" name="form1" method="post" action="ejecutaSelect.php">
+                <p>
+                    <label for="codigo">Codigo:</label>
+                    <input type="text" name="codigo" id="codigo" />
+                </p>
+                <p>
+                    <label for="descripcion">descripcion:</label>
+                    <input type="text" name="descripcion" id="descripcion" />
+                </p>
+                <p>
+                    <label for="direccion">direccion:</label>
+                    <input type="text" name="direccion" id="direccion" />
+                </p>
+                <p>
+                    <label for="telefono">telefono:</label>
+                    <input type="text" name="telefono" id="telefono" />
+                </p>
+                <p>
+                    <label for="ciudad">ciudad:</label>
+                    <input type="text" name="ciudad" id="ciudad" />
+                </p>
+                <p>
+                    <label for="provincia">provincia:</label>
+                    <input type="text" name="provincia" id="provincia" />
+                </p>
+                <p>
+                    <label for="email">email:</label>
+                    <input type="text" name="email" id="email" />
+                </p>
+                <p>
+                    <input
+                        type="submit"
+                        name="enviar"
+                        id="enviar"
+                        value="Enviar"
+                    />
+                </p>
+            </form>
+        </body>
+    </html>
+</html>
