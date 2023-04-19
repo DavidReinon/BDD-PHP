f3a6916838540ad775560743a4db3a9ef09bd044
DavidReinon <darega@floridauniversitaria.es>
2023-04-19 18:44:58 +0200

    cambio nombres

--- BD/Clase/Modificar y borrar/V2/MoB_producto.php
+++ BD/Clase/Modificar y borrar/V2/MoB_producto.php
@@ -27,17 +27,19 @@
     }
 
     while ($fila = $resultado->fetch_assoc()) {
-      echo "<p><label for='descripcion'>Descripcion</label>
+      echo /*html*/ "<p><label for='descripcion'>Descripcion</label>
     <input type='text' name='descripcion' id='descripcion' value='" . $fila['descripcionproducto'] . "' readonly='readonly'   />  </p>";
-      echo "<p><label for='proveedor'>Proveedor</label>
+
+      echo /*html*/"<p><label for='proveedor'>Proveedor</label>
     <input type='text' name='proveedor' id='proveedor' value='" . $fila['codigoproveedorproducto'] . "' readonly='readonly'   />  </p>";
-      echo "<p><label for='preciocompra'>Preciocompra</label>
+
+      echo /*html*/"<p><label for='preciocompra'>Preciocompra</label>
     <input type='text' name='preciocompra' id='preciocompra' value='" . $fila['preciocompraproducto'] . "' readonly='readonly'   />  </p>";
 
-      echo "<p><label for='precioventa'>Precioventa</label>
+      echo /*html*/"<p><label for='precioventa'>Precioventa</label>
     <input type='text' name='precioventa' id='precioventa' value='" . $fila['precioventaproducto'] . "' readonly='readonly'   />  </p>";
 
-      echo "<p><label for='stock'>Stock</label>
+      echo /*html*/"<p><label for='stock'>Stock</label>
     <input type='text' name='stock' id='stock' value='" . $fila['stockproducto'] . "' readonly='readonly'   />  </p>";
     }
     ?>
