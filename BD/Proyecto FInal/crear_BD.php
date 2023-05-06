144d4bea4ee66aa870f3b17b8ec816dd312f897f
darega <davidreinongarcia@gmail.com>
2023-05-06 17:29:02 +0200

    script crear BD

--- BD/Proyecto FInal/crear_BD.php
+++ BD/Proyecto FInal/crear_BD.php
@@ -13,24 +13,24 @@ $baseDatos = "academia";
 $consulta = "CREATE DATABASE IF NOT EXISTS $baseDatos;";
 ErrorConsulta($mysqli, $consulta);
 
-$mysqli->select_db($basedatos);
+$mysqli->select_db($baseDatos);
 
 $consulta = "CREATE TABLE IF NOT EXISTS alumnos ";
 $consulta .= "(dni CHAR(9),";
-$consulta .= "nombre VARCHAR(45), ";
+$consulta .= "nombre VARCHAR(45) NOT NULL, ";
 $consulta .= "apellidos VARCHAR(45), ";
-$consulta .= "telefono CHAR(9), ";
+$consulta .= "telefono CHAR(9) NOT NULL, ";
 $consulta .= "fechaNacimiento DATE, ";
 $consulta .= "PRIMARY KEY (dni)); ";
 ErrorConsulta($mysqli, $consulta);
 
 $consulta = "CREATE TABLE IF NOT EXISTS clases ";
 $consulta .= "(id INT AUTO_INCREMENT,";
-$consulta .= "dia DATE, ";
-$consulta .= "hora TIME, ";
+$consulta .= "dia DATE NOT NULL, ";
+$consulta .= "hora TIME NOT NULL, ";
 $consulta .= "sala VARCHAR(45), ";
-$consulta .= "materia VARCHAR(45) NOT NULL, ";
-$consulta .= "dniAlumno CHAR(9), ";
+$consulta .= "materia VARCHAR(45), ";
+$consulta .= "dniAlumno CHAR(9) NOT NULL, ";
 $consulta .= "FOREIGN KEY (dniAlumno) REFERENCES alumnos(dni), ";
 $consulta .= "PRIMARY KEY (id)); ";
 ErrorConsulta($mysqli, $consulta);
--- BD/Proyecto FInal/funciones.php
+++ BD/Proyecto FInal/funciones.php
@@ -4,7 +4,7 @@ function ErrorConexion($mysqli)
     if ($mysqli->connect_errno) {
         echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
         die("Salida del programa. Error acceso a BBDD");
-    } else echo "Se ha conectado al servidor MySQL<br>";
+    }
 }
 
 function ErrorConsulta($mysqli, $consulta)
