ef223a77f588ddadbd072fbe50f0ee1f20d90699
darega <davidreinongarcia@gmail.com>
2023-05-06 16:44:13 +0200

    archivo funciones

--- Proyecto FInal/crear_BD.php
+++ Proyecto FInal/crear_BD.php
@@ -1,15 +1,17 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR);
+require("funciones.php");
 
 $servidor = "localhost";
 $usuario = "root";
 $clave = "";
 
 @$mysqli = new mysqli($servidor, $usuario, $clave);
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    die("Salida del programa. Error acceso a BBDD");
-}
+ErrorConexion($mysqli);
 
-$basedatos = "gestiona";
+$baseDatos = "academia";
+
+$consulta = "CREATE DATABASE IF NOT EXISTS $baseDatos;";
+ErrorConsulta($mysqli, $consulta);
+
 $mysqli->select_db($basedatos);
---
+++ Proyecto FInal/funciones.php
@@ -0,0 +1,21 @@
+<?php
+function ErrorConexion($mysqli)
+{
+    if ($mysqli->connect_errno) {
+        echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        die("Salida del programa. Error acceso a BBDD");
+    } else echo "Se ha conectado al servidor MySQL<br>";
+}
+
+function ErrorConsulta($mysqli, $consulta)
+{
+    if (!$resultado = $mysqli->query($consulta)) {
+        echo "Lo sentimos. App falla<br>";
+        echo "Error en $consulta <br>";
+        echo "Num.error: " . $mysqli->errno . "<br>";
+        echo "Error: " . $mysqli->error . "<br>";
+        exit;
+    }
+    return $resultado;
+}
+
