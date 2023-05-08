217a246005ed5098c37dfb46386616f9ac6e5200
darega <davidreinongarcia@gmail.com>
2023-05-08 23:52:10 +0200

    estilo para mostrar datos clases

--- Proyecto Final/acciones/mostrar_datos_clases.php
+++ Proyecto Final/acciones/mostrar_datos_clases.php
@@ -5,23 +5,41 @@ $consulta = "SELECT * FROM clases;";
 $resultado = ErrorConsulta($mysqli, $consulta);
 
 $columns = mysqli_fetch_fields($resultado);
+?>
 
-echo "<table border = 1>";
-echo "<tr>";
-foreach ($columns as $column) {
-    echo "<th>" . $column->name . "</th>";
-}
-echo "</tr>";
+<!DOCTYPE html>
+<html lang="en">
 
-while ($fila = mysqli_fetch_array($resultado)) {
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/mostrar_datos_clases.css">
+    <title>Datos de CLASES</title>
+</head>
+
+<body>
+    <?php
+    echo "<table>";
     echo "<tr>";
-    echo "<td>" . $fila['id'] . "</td>";
-    echo "<td>" . $fila['dia'] . "</td>";
-    echo "<td>" . $fila['hora'] . "</td>";
-    echo "<td>" . $fila['sala'] . "</td>";
-    echo "<td>" . $fila['materia'] . "</td>";
-    echo "<td>" . $fila['dniAlumno'] . "</td>";
+    foreach ($columns as $column) {
+        echo "<th>" . $column->name . "</th>";
+    }
     echo "</tr>";
-}
 
-echo "</table>";
+    while ($fila = mysqli_fetch_array($resultado)) {
+        echo "<tr>";
+        echo "<td>" . $fila['id'] . "</td>";
+        echo "<td>" . $fila['dia'] . "</td>";
+        echo "<td>" . $fila['hora'] . "</td>";
+        echo "<td>" . $fila['sala'] . "</td>";
+        echo "<td>" . $fila['materia'] . "</td>";
+        echo "<td>" . $fila['dniAlumno'] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+    ?>
+</body>
+
+</html>
