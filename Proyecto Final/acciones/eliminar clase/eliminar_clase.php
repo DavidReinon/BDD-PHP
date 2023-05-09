d63f741fd299bed20a90113673eca6e80b938e23
darega <davidreinongarcia@gmail.com>
2023-05-10 00:19:07 +0200

    funcionalidad eliminar programada simple

--- Proyecto Final/acciones/eliminar clase/eliminar_clase.php
+++ Proyecto Final/acciones/eliminar clase/eliminar_clase.php
@@ -2,5 +2,21 @@
 mysqli_report(MYSQLI_REPORT_ERROR);
 require("../conexion_BD.php");
 
-$id=$_POST['id'];
+$id = $_POST['id'];
+//echo "Estas seguro de eliminar el siguiente registro";
+//Segunedo spreen
 $consulta = "DELETE FROM clases WHERE id = '$id'";
+
+$datos_consulta = ErrorConsulta($mysqli, $consulta);
+$resultado = $datos_consulta["resultado"];
+$error = $datos_consulta["bool"];
+
+if ($resultado && mysqli_affected_rows($mysqli) == 0) {
+    echo "No se ha podido ejecutar la eliminacion de este registro.";
+    echo "<br><a href='../mostrar_datos_clases.php'>Volver a el registro completo de clases</a>";
+    echo "<br><a href='../index.html'>Volver al index</a>";
+} elseif (!$error) {
+    echo "Registro eliminado correctamente";
+    echo "<br><a href='../mostrar_datos_clases.php'>Volver a el registro completo de clases</a>";
+    echo "<br><a href='../index.html'>Volver al index</a>";
+}
