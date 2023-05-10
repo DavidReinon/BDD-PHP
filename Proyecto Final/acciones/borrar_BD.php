7daad5e509addf75477a54b5879aa3024315c8a0
darega <davidreinongarcia@gmail.com>
2023-05-11 01:51:24 +0200

    borrar_BD mejorado en caso de error

--- Proyecto Final/acciones/borrar_BD.php
+++ Proyecto Final/acciones/borrar_BD.php
@@ -4,14 +4,19 @@ require("conexion_BD.php");
 $confirmacion = $_POST["confirmacion"];
 
 if ($confirmacion != "borrar") {
-    echo "<h2>La base de datos no ha sido borrada, siga los pasos correctos para ejecutar el borrado</h2>";
+    echo "<h2>La base de datos no ha sido borrada, siga los pasos correctamente para ejecutar el borrado</h2>";
     echo "<a href='../index.html'><button type='button'>Volver al inicio</button></a>";
     die();
 }
 
 $database = "academia";
 $consulta = "DROP DATABASE $database;";
-ErrorConsulta($mysqli, $consulta);
 
+$error = ErrorConsulta($mysqli, $consulta)["bool"];
+
+if ($error) {
+    echo "<br><a href='../index.html'><button type='button'>Volver al index</button></a>";
+    die();
+}
 echo "<h1>Base de datos borrada</h1>";
-echo "<a href='../index.html'><button type='button'>Volver al inicio</button></a>";
+echo "<a href='../index.html'><button type='button'>Volver al index</button></a>";
