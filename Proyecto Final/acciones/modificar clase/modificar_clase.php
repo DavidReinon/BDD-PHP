e4139adb6ba127e92a48f932457295033cae66d0
darega <davidreinongarcia@gmail.com>
2023-05-14 20:43:13 +0200

    añadido filtro de hora,dia y sala para añadir clase

--- Proyecto Final/acciones/modificar clase/modificar_clase.php
+++ Proyecto Final/acciones/modificar clase/modificar_clase.php
@@ -4,24 +4,27 @@ require("../conexion_BD.php");
 
 $id = $_POST['id'];
 
-$dia = $_REQUEST["dia"];
-$hora = $_REQUEST["hora"];
-$sala = $_REQUEST["sala"];
-$materia = $_REQUEST["materia"];
-$dni = $_REQUEST["dni"];
+$dia = $_POST["dia"];
+$hora = $_POST["hora"];
+$sala = $_POST["sala"];
+$materia = $_POST["materia"];
+$dni = $_POST["dni"];
 
-$consulta = "UPDATE clases SET dia='$dia',hora='$hora',sala='$sala',materia='$materia',dniAlumno='$dni' where id='$id';";
+$consulta = "UPDATE clases SET dia='$dia',hora='$hora',sala='$sala',materia='$materia',dniAlumno='$dni' 
+where id='$id' 
+AND NOT EXISTS (
+    SELECT 1 FROM clases
+    WHERE sala = '$sala' AND dia = '$dia' AND hora = '$hora'
+);";
 
 $datos_consulta = ErrorConsulta($mysqli, $consulta);
 $resultado = $datos_consulta["resultado"];
 $error = $datos_consulta["bool"];
 
 if ($resultado && mysqli_affected_rows($mysqli) == 0) {
-    echo "No se ha podido ejecutar la modificacion de este registro.";
-    echo "<br><a href='formulario_modificar.php'>Volver a el formulario de modificacion</a>";
+    echo "Ya existe una clase resgistrada con los datos que has introducido (Dia, hora o sala tienen que ser distintos)";
 } elseif (!$error) {
     echo "Registro modificado correctamente";
-
 }
-echo "<br><a href='../mostrar_datos_alumnos.php'>Volver a el registro completo de alumnos</a>";
+echo "<br><a href='../mostrar_datos_clases.php'>Volver a el registro completo de clases</a>";
 echo "<br><a href='../index.html'>Volver al index</a>";
